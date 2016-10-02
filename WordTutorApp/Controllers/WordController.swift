7a05434c2d01d9b030cad58218216bfc7bab0e42
Andrey Vokin <andrey.vokin@gmail.com>
2016-10-02 08:18:39 +0200

    Skip missing groups for Word view

--- WordTutorApp/Controllers/WordController.swift
+++ WordTutorApp/Controllers/WordController.swift
@@ -6,17 +6,37 @@
 class WordController: UITableViewController {
     var word: Word? = nil
 
+    var translations_section = -1
+    var related_words_section = -1
+    var comment_section = -1
+    var section_count = 0
+
     override func viewDidLoad() {
         super.viewDidLoad()
         self.tableView.estimatedRowHeight = 44.0
         self.tableView.rowHeight = UITableViewAutomaticDimension
         self.title = word!.text
+
+        if self.word!.translations.count > 0 {
+            self.translations_section = 0
+            self.section_count += 1
+        }
+
+        if self.word!.synonyms.count > 0 {
+            self.related_words_section = self.section_count
+            self.section_count += 1
+        }
+
+        if self.word!.comment.characters.count > 0 {
+            comment_section = self.section_count
+            self.section_count += 1
+        }
     }
 
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        if section == 0 {
+        if section == translations_section {
             return word!.translations.count
-        } else if section == 1 {
+        } else if section == related_words_section {
             return word!.synonyms.count
         } else {
             if word!.comment.characters.count > 0 {
@@ -27,14 +47,14 @@ class WordController: UITableViewController {
     }
 
     override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-        return 3
+        return self.section_count
     }
 
     override internal func tableView(tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-        if section == 0 {
+        if section == translations_section {
             return "Translations"
         }
-        if section == 1 {
+        if section == related_words_section {
             return "Related words"
         }
         return "Comment"
@@ -43,14 +63,22 @@ class WordController: UITableViewController {
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCellWithIdentifier("Word", forIndexPath: indexPath)
 
-        if indexPath.section == 0 {
+        if indexPath.section == translations_section {
             cell.textLabel!.text = word!.translations[indexPath.row].text
-        } else if indexPath.section == 1 {
+            cell.tag = word!.translations[indexPath.row].id
+        } else if indexPath.section == related_words_section {
             cell.textLabel!.text = word!.synonyms[indexPath.row].text
+            cell.tag = word!.synonyms[indexPath.row].id
         } else {
             cell.textLabel!.text = word!.comment
+            cell.tag = 0
         }
 
         return cell
+    }
+
+    @available(iOS 5.0, *) override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        let wordController = segue.destinationViewController as! WordController
+        wordController.word = Word.ids[(sender as! UITableViewCell).tag]
     }
 }
