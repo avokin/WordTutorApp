24853422630b74496d333a7f37549f5acbaeca70
Andrey Vokin <andrey.vokin@gmail.com>
2016-11-06 15:38:20 +0100

    Tap to fail

--- WordTutorApp/Controllers/WordTrainingController.swift
+++ WordTutorApp/Controllers/WordTrainingController.swift
@@ -5,7 +5,7 @@
 
 import Foundation
 
-class WordTrainingController: UIViewController {
+class WordTrainingController: UIViewController, UIGestureRecognizerDelegate {
     @IBOutlet weak var lblWord: UILabel!
     @IBOutlet weak var lblTranslations: UILabel!
     @IBOutlet weak var lblGrammar: UILabel!
@@ -17,12 +17,21 @@ class WordTrainingController: UIViewController {
     var leftSibling: WordTrainingController?
     var rightSibling: WordTrainingController?
 
+    var isFailed = false
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        let tap = UITapGestureRecognizer(target: self, action: Selector("handleTap"))
+        tap.delegate = self
+        view.addGestureRecognizer(tap)
+    }
+
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
 
         initTest()
     }
-
 
     func initTest() {
         (parent as! TrainingController).currentIndex = wordIndex
@@ -30,32 +39,39 @@ class WordTrainingController: UIViewController {
         lblGrammar.text = ""
         lblTranslations.text = ""
         lblComment.text = ""
+        isFailed = false
     }
 
     @IBAction func rememberAction(_ sender: AnyObject) {
         word!.setSuccessCount(value: word!.getSuccessCount() + 1)
         (parent as! TrainingController).showNextController()
     }
-    
-    @IBAction func forgotAction(_ sender: AnyObject) {
-        if word!.getTypeId() == 2 {
-            var grammar = ""
-            if word!.getCustomIntField1() == 1 {
-                grammar = "Maskulin"
-            } else if word!.getCustomIntField1() == 2 {
-                grammar = "Neutrum"
-            } else if word!.getCustomIntField1() == 3 {
-                grammar = "Femininum"
-            }
-            if word!.getCustomStringField1().characters.count > 0 {
-                grammar = grammar + " (" + word!.getCustomStringField1() + ")"
-            }
 
-            lblGrammar.text = grammar
+    public func handleTap() {
+        if !isFailed {
+            isFailed = true
+
+            if word!.getTypeId() == 2 {
+                var grammar = ""
+                if word!.getCustomIntField1() == 1 {
+                    grammar = "Maskulin"
+                } else if word!.getCustomIntField1() == 2 {
+                    grammar = "Neutrum"
+                } else if word!.getCustomIntField1() == 3 {
+                    grammar = "Femininum"
+                }
+                if word!.getCustomStringField1().characters.count > 0 {
+                    grammar = grammar + " (" + word!.getCustomStringField1() + ")"
+                }
+
+                lblGrammar.text = grammar
+            }
+            lblTranslations.text = word!.getTranslations()
+            lblComment.text = word!.getComment()
+
+            word!.setSuccessCount(value: 0)
+        } else {
+            (parent as! TrainingController).showNextController()
         }
-        lblTranslations.text = word!.getTranslations()
-        lblComment.text = word!.getComment()
-
-        word!.setSuccessCount(value: 0)
     }
 }
