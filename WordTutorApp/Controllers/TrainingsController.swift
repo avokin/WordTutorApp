468da5cb3b265d878258846fb6afceb952807138
Andrey Vokin <andrey.vokin@gmail.com>
2016-11-09 22:29:41 +0100

    UI cleanup

--- WordTutorApp/Controllers/TrainingsController.swift
+++ WordTutorApp/Controllers/TrainingsController.swift
@@ -17,10 +17,6 @@ class TrainingsController: UITableViewController {
         menuButton.target = revealViewController()
         menuButton.action = #selector(SWRevealViewController.revealToggle(_:))
 
-        revealViewController().rightViewRevealWidth = 150
-        extraButton.target = revealViewController()
-        extraButton.action = #selector(SWRevealViewController.rightRevealToggle(_:))
-
         DataProvider.getInstance().serviceResponse = serviceResponseCallback
 
         tableView.register(UINib(nibName: "TrainingItemCell", bundle: nil), forCellReuseIdentifier: "TrainingItemCell")
